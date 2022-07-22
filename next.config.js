8c5bca544f2a63c745bfea4f7f2d1301d596ba6d
juandtr <juandtr391@gmail.com>
2022-07-22 01:54:49 -0500

    actualizacion 8

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+      customKey: 'https://1jn3kg5xe5.execute-api.us-east-1.amazonaws.com/dev',
+    },
+  }
+  
+  module.exports = nextConfig
